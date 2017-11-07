4b83f05f705d0c2b4b098fdad7f963af16110035
Philipp Hadjimina <philipp.hadjimina@protonmail.com>
2017-11-07 13:21:31 +0100

    Initial commit

---
+++ app/src/main/java/com/example/philipp/timesup/CreateJoinActivity.java
@@ -0,0 +1,13 @@
+package com.example.philipp.timesup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateJoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_join);
+    }
+}
