eab450592220362506dd7c89c6313818dc2406bf
schwabea <schwabea@student.ethz.ch>
2017-11-16 13:56:58 +0100

    finish wordsactivity as far as possible

--- app/src/main/java/com/example/philipp/timesup/WordsActivity.java
+++ app/src/main/java/com/example/philipp/timesup/WordsActivity.java
@@ -1,10 +1,12 @@
 package com.example.philipp.timesup;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by MammaGiulietta on 11.11.17.
@@ -17,11 +19,16 @@ import android.widget.Button;
  Lets players input specified number of words
  */
 
-public class WordsActivity extends AppCompatActivity {
+public class WordsActivity extends ServerIOActivity {
     SharedPreferences prefs;
     int wordsPerPerson;
-    SharedPreferences.Editor editor;
     String [] wordsArray;
+    int counter = 0;
+    Button enterButton;
+    Button readyButton;
+    EditText editText;
+    Boolean readyAnswer = false;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +36,53 @@ public class WordsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_words);
 
 
-
+        //get WordsPerPerson from shared preferences
         prefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
-        wordsPerPerson = prefs.getInt("wordsPerPerson", 0);
-        wordsArray = new String[wordsPerPerson];
-        editor = prefs.edit();
+        wordsPerPerson = prefs.getInt("wordsPerPerson", 5);
 
-        Button enterbutton = findViewById(R.id.button_enter);
-        enterbutton.setOnClickListener(new View.OnClickListener() {
+        //initialize array for words
+        wordsArray = new String[wordsPerPerson];
+
+
+        //initialize enterbutton and editext
+        enterButton = findViewById(R.id.button_enter);
+        editText = findViewById(R.id.enter_word_edit);
+
+        //set on click listener for enterbutton
+        enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // array isn't full yet
-                if (wordsArray[wordsPerPerson].equals(null)){
-                    //TODO add new word
+                if (counter <  wordsPerPerson){
+
+                    //add word to array and clear edittext
+                    wordsArray[counter] = String.valueOf(editText.getText());
+                    editText.setText("");
+                    counter++;
                 }
                 //array is full, so send message to server
                 else {
+                    enterButton.setVisibility(View.INVISIBLE);
+                    readyButton.setVisibility(View.VISIBLE);
+                    //TODO send message to server with request type unready and gameid, clientid (shouldn't it be words list here?)
+                }
+            }
+        });
+
+        //initialize readybutton
+        readyButton = findViewById(R.id.button_ready);
+
+        //set onclicklistener for readybutton
+        readyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (readyAnswer){
+                    intent = new Intent(getApplicationContext(), GameEndActivity.class);
+
                     //TODO send message to server
+                }
+                else {
+                    //do nothing
                 }
             }
         });
@@ -53,6 +90,27 @@ public class WordsActivity extends AppCompatActivity {
 
 
 
+
+    }
+
+    @Override
+    public void callback(DecodeMessage message) {
+
+        //if callback from unready, set global variable to true so that user can click on the readybutton
+        if (message.getRequestType().equals("unready") && message.getReturnType().equals("ACK")){
+            readyAnswer = true;
+        }
+        else if (message.getRequestType().equals("unready") && message.getReturnType().equals("error")){
+            //TODO errorhandling
+        }
+        else if (message.getRequestType().equals("ready") && message.getReturnType().equals("ACK")){
+
+            //get from message which role you will have
+            startActivity(intent);
+        }
+        else if (message.getRequestType().equals("ready") && message.getReturnType().equals("error")){
+            //TODO errorhandling
+        }
 
     }
 }
