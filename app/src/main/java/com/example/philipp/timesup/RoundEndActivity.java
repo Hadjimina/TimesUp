de5145a066680c3934743da1631374b20b9ee749
David <davidn@student.ethz.ch>
2017-12-15 10:50:19 +0100

    Hallo

--- app/src/main/java/com/example/philipp/timesup/RoundEndActivity.java
+++ app/src/main/java/com/example/philipp/timesup/RoundEndActivity.java
@@ -44,6 +44,9 @@ public class RoundEndActivity extends ServerIOActivity  {
         Log.d("#RoundEndActivity", "RoundEndActivity is beeing created");
         setCallbackActivity(this);
 
+        //if roundFinished is not received, this will no interfere with the clientId 0
+        nextPlayerId = -1;
+
 
 
         //initialize global variables from shared static NetworkHelper Class
